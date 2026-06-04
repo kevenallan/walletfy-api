86d142daeddc6a79562ee665db5591c8bc9ec81b
Keven Allan <kevenallan98@gmail.com>
2026-06-03 21:46:38 -0300

    initial commit

---
+++ src/main/java/br/com/walletfy/WalletfyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.walletfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletfyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletfyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/walletfy/WalletfyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walletfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletfyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
