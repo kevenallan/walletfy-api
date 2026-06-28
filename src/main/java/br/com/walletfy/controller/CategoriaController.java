9d27acb543e373372ca50229e9b6cc8a9a1b59d9
Keven Allan <kevenallan98@gmail.com>
2026-06-27 23:14:20 -0300

    UPDATE-Task-83 - Adiciona funcionalidade de deletar categoria e atualiza os metodo para utilizar o tipo

--- src/main/java/br/com/walletfy/controller/CategoriaController.java
+++ src/main/java/br/com/walletfy/controller/CategoriaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,11 @@ public class CategoriaController {
     public ResponseEntity<CategoriaResponseDTO> atualizar(@Valid @RequestBody CategoriaRequestDTO categoria) {
         return  ResponseEntity.status(HttpStatus.OK)
         		.body(this.categoriaService.atualizar(this.usuarioId(), categoria));
+    }
+
+    @DeleteMapping("/{categoriaId}")
+    public ResponseEntity<Void> deletar(@PathVariable Long categoriaId) {
+    	this.categoriaService.deletar(this.usuarioId(), categoriaId);
+        return  ResponseEntity.noContent().build();
     }
 }
